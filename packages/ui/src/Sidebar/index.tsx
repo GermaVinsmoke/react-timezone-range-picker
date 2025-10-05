4f99ecd410292b8f029aebeb28ba7c5b329eff8a
GermaVinsmoke <vaibhav1180@gmail.com>
2025-10-06 01:02:22 +0900

    ui: add panel toggle

--- packages/ui/src/Sidebar/index.tsx
+++ packages/ui/src/Sidebar/index.tsx
@@ -1,19 +1,25 @@
 import { Box, Flex, Text } from "@mantine/core";
 import { IconClock, IconCalendarWeekFilled, IconWorld } from "@tabler/icons-react";
 import styled from "./index.module.css";
+import { Dispatch, FC, SetStateAction } from "react";
+import { Panel } from "../TimezoneRangePicker";
 
-export const Sidebar = () => {
+interface ISidebar {
+  setSelectedPanel: Dispatch<SetStateAction<Panel>>;
+}
+
+export const Sidebar: FC<ISidebar> = ({ setSelectedPanel }) => {
   return (
     <Flex direction="column" className={styled["sidebar-container"]} flex={1}>
-      <Box className={styled["sidebar-row"]}>
+      <Box className={styled["sidebar-row"]} onClick={() => setSelectedPanel(Panel.START_END_TIME)}>
         <IconCalendarWeekFilled />
         <Text>Start and end times</Text>
       </Box>
-      <Box className={styled["sidebar-row"]}>
+      <Box className={styled["sidebar-row"]} onClick={() => setSelectedPanel(Panel.AROUND_TIME)}>
         <IconClock />
         <Text>Around a time</Text>
       </Box>
-      <Box className={styled["sidebar-row"]}>
+      <Box className={styled["sidebar-row"]} onClick={() => setSelectedPanel(Panel.TIMEZONE)}>
         <IconWorld />
         <Text>Time zone</Text>
       </Box>
--- packages/ui/src/TimezoneRangePicker/index.tsx
+++ packages/ui/src/TimezoneRangePicker/index.tsx
@@ -2,8 +2,32 @@ import { Flex, Menu, Popover, Text } from "@mantine/core";
 import { Button } from "@mantine/core";
 import { TimezonePanel } from "../TimezonePanel";
 import { Sidebar } from "../Sidebar";
+import { AroundTimePanel } from "../AroundTimePanel";
+import { StartEndTimePanel } from "../StartEndTimePanel";
+import { useState } from "react";
+
+export enum Panel {
+  START_END_TIME = "Start and end times",
+  AROUND_TIME = "Around a time",
+  TIMEZONE = "Time zone",
+}
 
 export const TimezoneRangePicker = () => {
+  const [selectedPanel, setSelectedPanel] = useState<Panel>(Panel.START_END_TIME);
+
+  const renderPanel = () => {
+    switch (selectedPanel) {
+      case Panel.START_END_TIME:
+        return <StartEndTimePanel />;
+      case Panel.AROUND_TIME:
+        return <AroundTimePanel />;
+      case Panel.TIMEZONE:
+        return <TimezonePanel />;
+      default:
+        return null;
+    }
+  };
+
   return (
     <Popover position="bottom" shadow="md">
       <Popover.Target>
@@ -11,8 +35,8 @@ export const TimezoneRangePicker = () => {
       </Popover.Target>
       <Popover.Dropdown style={{ width: 600, height: 400 }}>
         <Flex style={{ height: "100%" }}>
-          <Sidebar />
-          <TimezonePanel />
+          <Sidebar setSelectedPanel={setSelectedPanel} />
+          {renderPanel()}
         </Flex>
       </Popover.Dropdown>
     </Popover>
