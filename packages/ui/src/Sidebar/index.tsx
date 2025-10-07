b0f5c181ddbb8726c8e56a68cf26f71c52792098
GermaVinsmoke <vaibhav1180@gmail.com>
2025-10-07 10:39:21 +0900

    ui: sidebar style changes

--- packages/ui/src/Sidebar/index.tsx
+++ packages/ui/src/Sidebar/index.tsx
@@ -1,4 +1,4 @@
-import { Flex, Text } from "@mantine/core";
+import { Box, Flex, Text } from "@mantine/core";
 import {
   IconClock,
   IconCalendarWeekFilled,
@@ -8,6 +8,7 @@ import {
 import styled from "./index.module.css";
 import { Dispatch, FC, SetStateAction } from "react";
 import { Panel } from "../TimezoneRangePicker";
+import { RelativeTimePanel } from "../RelativeTimePanel";
 
 interface ISidebar {
   setSelectedPanel: Dispatch<SetStateAction<Panel>>;
@@ -15,43 +16,46 @@ interface ISidebar {
 
 export const Sidebar: FC<ISidebar> = ({ setSelectedPanel }) => {
   return (
-    <Flex direction="column" className={styled["sidebar-container"]} flex={1}>
-      <Flex
-        className={styled["sidebar-row"]}
-        justify={"space-between"}
-        align={"center"}
-        onClick={() => setSelectedPanel(Panel.START_END_TIME)}
-      >
-        <Flex>
-          <IconCalendarWeekFilled stroke={1.5} />
-          <Text>Start and end times</Text>
+    <Box className={styled["sidebar-container"]} flex={1}>
+      <RelativeTimePanel />
+      <Flex direction="column" rowGap={16} py={8} style={{ borderTop: "1px solid #ddd" }}>
+        <Flex
+          className={styled["sidebar-row"]}
+          justify={"space-between"}
+          align={"center"}
+          onClick={() => setSelectedPanel(Panel.START_END_TIME)}
+        >
+          <Flex align={"center"}>
+            <IconCalendarWeekFilled stroke={1.5} />
+            <Text size="sm">Start and end times</Text>
+          </Flex>
+          <IconChevronRight stroke={1.5} />
         </Flex>
-        <IconChevronRight stroke={1.5} />
-      </Flex>
-      <Flex
-        className={styled["sidebar-row"]}
-        justify={"space-between"}
-        align={"center"}
-        onClick={() => setSelectedPanel(Panel.AROUND_TIME)}
-      >
-        <Flex>
-          <IconClock stroke={1.5} />
-          <Text>Around a time</Text>
+        <Flex
+          className={styled["sidebar-row"]}
+          justify={"space-between"}
+          align={"center"}
+          onClick={() => setSelectedPanel(Panel.AROUND_TIME)}
+        >
+          <Flex align={"center"}>
+            <IconClock stroke={1.5} />
+            <Text size="sm">Around a time</Text>
+          </Flex>
+          <IconChevronRight stroke={1.5} />
         </Flex>
-        <IconChevronRight stroke={1.5} />
-      </Flex>
-      <Flex
-        className={styled["sidebar-row"]}
-        justify={"space-between"}
-        align={"center"}
-        onClick={() => setSelectedPanel(Panel.TIMEZONE)}
-      >
-        <Flex>
-          <IconWorld stroke={1.5} />
-          <Text>Time zone</Text>
+        <Flex
+          className={styled["sidebar-row"]}
+          justify={"space-between"}
+          align={"center"}
+          onClick={() => setSelectedPanel(Panel.TIMEZONE)}
+        >
+          <Flex align={"center"}>
+            <IconWorld stroke={1.5} />
+            <Text size="sm">Time zone</Text>
+          </Flex>
+          <IconChevronRight stroke={1.5} />
         </Flex>
-        <IconChevronRight stroke={1.5} />
       </Flex>
-    </Flex>
+    </Box>
   );
 };
