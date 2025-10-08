f1b98d1b0fcc5fb25d160326c52ff08476a15715
GermaVinsmoke <vaibhav1180@gmail.com>
2025-10-08 11:53:37 +0900

    ui: add theme to playground to test app based on light and dark theme

--- apps/playground/App.tsx
+++ apps/playground/App.tsx
@@ -2,12 +2,19 @@ import {
   TimezoneRangePicker,
   TimezonePanel,
 } from "@react-timezone-range-picker/ui";
+import { Button, useMantineColorScheme, useMantineTheme } from "@mantine/core";
 
 function App() {
+  const { colorScheme, setColorScheme } = useMantineColorScheme();
+
+  const toggle = () =>
+    setColorScheme(colorScheme === "dark" ? "light" : "dark");
+
   return (
     <div>
       <TimezoneRangePicker />
       {/* <TimezonePanel /> */}
+      <Button onClick={toggle}>Toggle Theme</Button>
     </div>
   );
 }
