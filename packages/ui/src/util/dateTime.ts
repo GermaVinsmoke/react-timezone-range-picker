d153d8e691428748f7753bd0b3cb9205ce399a57
GermaVinsmoke <vaibhav1180@gmail.com>
2025-10-10 14:25:43 +0900

    add util fns to get short timezone and popover button text

--- packages/ui/src/util/dateTime.ts
+++ packages/ui/src/util/dateTime.ts
@@ -1,4 +1,5 @@
 import dayjs from "dayjs";
+import { TzRange } from "../interfaces";
 
 export const getCurrentTime = () => dayjs().format("HH:mm:ss");
 
@@ -6,3 +7,14 @@ export const getCurrentDate = () => dayjs().format("YYYY-MM-DD");
 
 export const getCurrentPlusDate = (amount: number) =>
   dayjs().add(amount, "day").format("YYYY-MM-DD");
+
+export const getShortTimezoneName = (tz: string | null) => {
+  if (!tz) return tz;
+  return dayjs().tz(tz).format("Z");
+};
+
+export const getPopoverButtonText = (tzRange: TzRange) => {
+  return `${dayjs(tzRange.startDate).format("YYYY/MM/DD")} ${tzRange.startTime} - ${dayjs(
+    tzRange.endDate
+  ).format("YYYY/MM/DD")} ${tzRange.endTime} (${getShortTimezoneName(tzRange.timezone.name)})`;
+};
