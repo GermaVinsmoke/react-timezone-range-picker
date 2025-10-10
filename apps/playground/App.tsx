52f616468950b25421ab310e89aa4bd0d076f794
GermaVinsmoke <vaibhav1180@gmail.com>
2025-10-10 13:48:10 +0900

    implement package usage in playground app with init state

--- apps/playground/App.tsx
+++ apps/playground/App.tsx
@@ -1,6 +1,7 @@
 import {
   TimezoneRangePicker,
   TimezoneData,
+  OnApplyParams,
 } from "@react-timezone-range-picker/ui";
 import { Box, Button, Flex, Text, useMantineColorScheme } from "@mantine/core";
 import { useState } from "react";
@@ -11,15 +12,63 @@ import timezone from "dayjs/plugin/timezone";
 dayjs.extend(utc);
 dayjs.extend(timezone);
 
+const getCurrentTime = () => dayjs().format("HH:mm:ss");
+
+const getCurrentDate = () => dayjs().format("YYYY-MM-DD");
+
+const getCurrentPlusDate = (amount: number) =>
+  dayjs().add(amount, "day").format("YYYY-MM-DD");
+
 const DEFAULT_TIMEZONE = {
   name: "Asia/Tokyo",
   longName: "Japan Standard Time",
   utcOffset: "+09:00",
 };
 
+const DEFAULT_RANGE = {
+  startDate: getCurrentDate(),
+  startTime: getCurrentTime(),
+  endDate: getCurrentPlusDate(30),
+  endTime: getCurrentTime(),
+  timezone: DEFAULT_TIMEZONE,
+};
+
+const EMPTY_RANGE = {
+  startDate: null,
+  startTime: null,
+  endDate: null,
+  endTime: null,
+  timezone: {
+    name: null,
+    longName: null,
+    utcOffset: null,
+  },
+};
+
+type TzRange = {
+  startDate: string | null;
+  startTime: string | null;
+
+  endDate: string | null;
+  endTime: string | null;
+
+  timezone: TimezoneData;
+};
+
 function App() {
-  const [timezone, setTimezone] = useState<TimezoneData>(DEFAULT_TIMEZONE);
+  const [range, setRange] = useState<TzRange>(DEFAULT_RANGE);
+
   const { colorScheme, setColorScheme } = useMantineColorScheme();
+
+  const handleTimeRangeApply = ({
+    startDate,
+    startTime,
+    endDate,
+    endTime,
+    timezone,
+  }: OnApplyParams) => {
+    setRange({ startDate, startTime, endDate, endTime, timezone });
+  };
 
   const toggle = () =>
     setColorScheme(colorScheme === "dark" ? "light" : "dark");
@@ -27,13 +76,23 @@ function App() {
   return (
     <Box>
       <Text>
-        Selected Timezone: {timezone.name} ({timezone.longName})
+        Selected Timezone: {range.timezone.name} ({range.timezone.longName})
       </Text>
       <Text>
-        Current Time: {dayjs().tz(timezone.name).format("YYYY-MM-DD HH:mm:ss")}
+        Current Time:{" "}
+        {range.timezone.name &&
+          dayjs().tz(range.timezone.name).format("YYYY-MM-DD HH:mm:ss")}
       </Text>
+      <Text>UTC Offset: {range.timezone.utcOffset}</Text>
+      <Text>
+        Start: {range.startDate} {range.startTime}
+      </Text>
+      <Text>
+        End: {range.endDate} {range.endTime}
+      </Text>
+      <br />
       <Flex columnGap={8}>
-        <TimezoneRangePicker timezone={timezone} setTimezone={setTimezone} />
+        <TimezoneRangePicker {...range} onApply={handleTimeRangeApply} />
         <Button onClick={toggle}>Toggle Theme</Button>
       </Flex>
     </Box>
