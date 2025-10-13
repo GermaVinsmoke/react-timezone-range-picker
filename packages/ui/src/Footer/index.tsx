03fcd9a112131a55dea630793e0b19a9b936d031
GermaVinsmoke <vaibhav1180@gmail.com>
2025-10-13 20:22:28 +0900

    implement close popover using context

--- packages/ui/src/Footer/index.tsx
+++ packages/ui/src/Footer/index.tsx
@@ -1,19 +1,15 @@
 import { Button, Flex } from "@mantine/core";
 import { useAppTheme } from "../hooks/useAppTheme";
-import { FC } from "react";
+import { usePopoverContext } from "../Provider/PopoverProvider";
 
-interface IFooter {
-  handleApplyClick?: () => void;
-  handleCancelClick?: () => void;
-}
-
-export const Footer: FC<IFooter> = ({ handleCancelClick }) => {
+export const Footer = () => {
   const { theme } = useAppTheme();
+  const { closePopover } = usePopoverContext();
 
   return (
     <Flex justify={"flex-end"}>
       <Flex columnGap={16}>
-        <Button size="xs" variant="subtle" color={theme.primaryColor} onClick={handleCancelClick}>
+        <Button size="xs" variant="subtle" color={theme.primaryColor} onClick={closePopover}>
           Cancel
         </Button>
         <Button type="submit" size="xs" color={theme.primaryColor}>
