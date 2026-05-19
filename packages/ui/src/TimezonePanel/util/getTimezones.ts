7769c633e4d238fd4de09fc07c538b29a9a3ad7e
GermaVinsmoke <vaibhav1180@gmail.com>
2026-05-19 18:10:55 +0900

    add utc as explicit fixed timezone option

--- packages/ui/src/TimezonePanel/util/getTimezones.ts
+++ packages/ui/src/TimezonePanel/util/getTimezones.ts
@@ -7,20 +7,27 @@ import { parseOffset } from "./parseOffest";
 dayjs.extend(utc);
 dayjs.extend(timezone);
 
+const buildTimezoneOption = (timezone: string) => {
+  const offset = dayjs().tz(timezone).format("Z");
+
+  return {
+    name: timezone,
+    longName: longTzName(timezone),
+    currentTime: dayjs().tz(timezone).format("h:mm A"),
+    utcOffset: offset,
+    offsetMinutes: parseOffset(offset),
+  };
+};
+
 export const getTimezones = () => {
   const timezones = Intl.supportedValuesOf("timeZone");
 
-  return timezones
-    .map((timezone) => {
-      const offset = dayjs().tz(timezone).format("Z");
-
-      return {
-        name: timezone,
-        longName: longTzName(timezone),
-        currentTime: dayjs().tz(timezone).format("h:mm A"),
-        utcOffset: offset,
-        offsetMinutes: parseOffset(offset),
-      };
-    })
-    .sort((a, b) => a.offsetMinutes - b.offsetMinutes);
+  return ["UTC", ...timezones]
+    .filter((timezone, index, list) => list.indexOf(timezone) === index)
+    .map((timezone) => buildTimezoneOption(timezone))
+    .sort((a, b) => {
+      if (a.name === "UTC") return -1;
+      if (b.name === "UTC") return 1;
+      return a.offsetMinutes - b.offsetMinutes;
+    });
 };
