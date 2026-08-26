11cd4eada05f6d8ec8f8bcae1ef9541e7033f5a2
GermaVinsmoke <vaibhav1180@gmail.com>
2026-08-27 02:26:13 +0900

    test: configure Mantine component environment

--- packages/ui/src/test.setup.ts
+++ packages/ui/src/test.setup.ts
@@ -1,1 +1,15 @@
 import "@testing-library/jest-dom/vitest"; // extends expect with jest-dom matchers
+
+Object.defineProperty(window, "matchMedia", {
+  writable: true,
+  value: (query: string) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: () => undefined,
+    removeListener: () => undefined,
+    addEventListener: () => undefined,
+    removeEventListener: () => undefined,
+    dispatchEvent: () => false,
+  }),
+});
